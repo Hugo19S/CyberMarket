990ffb6b2ee9a214c64575f1eb4ab6cd10e58514
Hugo19S <adyseta1998@outlook.com>
2023-11-24 17:20:05 +0000

    pagina do home em construção

--- resources/views/components/admin_components/home.blade.php
+++ resources/views/components/admin_components/home.blade.php
@@ -9,13 +9,32 @@
     <div class="last_additions">
         <h3>Últimos 5 produtos adicionados</h3>
         <div class="additions">
-            <div class="productDetails">
-                <img src="" alt="">
-            </div>
+            @for ($i = 1; $i <= 5; $i++) <div class="productDetails">
+                <img src="/img/laptops.png" alt="">
         </div>
+        @endfor
     </div>
-    <div class="last_orders"></div>
-    <div class="analytics"></div>
+</div>
+<div class="last_orders">
+    <h3>Últimos 5 pedidos</h3>
+    <div class="orders">
+        @for ($i = 1; $i <= 5; $i++) 
+            <div class="productDetails">
+                <img src="/img/laptops.png" alt="">
+            </div>
+        @endfor
+</div>
+</div>
+<div class="analytics_info">
+    <h3>Analise de visita as páginas</h3>
+    <div class="analytics">
+        {{-- <div class="orderDetails">
+            <img src="/img/laptops.png" alt="">
+        </div>
+        --}}
+
+    </div>
+</div>
 
 
 </div>
